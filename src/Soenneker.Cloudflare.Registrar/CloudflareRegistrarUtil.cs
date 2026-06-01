e69d72ed08233f9e72cadd676d33c4e5d8e6f1c6
Jake Soenneker <jake@soenneker.com>
2026-06-01 12:03:29 -0500

    format

--- src/Soenneker.Cloudflare.Registrar/CloudflareRegistrarUtil.cs
+++ src/Soenneker.Cloudflare.Registrar/CloudflareRegistrarUtil.cs
@@ -31,16 +31,18 @@ public sealed class CloudflareRegistrarUtil : ICloudflareRegistrarUtil
     }
 
     public async ValueTask<RegistrarApiDomainSearchResponse?> SearchDomains(string accountId,
-        Action<RequestConfiguration<DomainSearchRequestBuilder.DomainSearchRequestBuilderGetQueryParameters>>? requestConfiguration = null,
-        CancellationToken cancellationToken = default)
+        Action<RequestConfiguration<DomainSearchRequestBuilder.DomainSearchRequestBuilderGetQueryParameters>>?
+            requestConfiguration = null, CancellationToken cancellationToken = default)
     {
         RegistrarRequestBuilder registrar = await Get(accountId, cancellationToken).NoSync();
 
         return await registrar.DomainSearch.GetAsync(requestConfiguration, cancellationToken).NoSync();
     }
 
-    public async ValueTask<RegistrarApiDomainCheckResponse?> CheckDomain(string accountId, RegistrarApiDomainCheckRequest request,
-        Action<RequestConfiguration<DefaultQueryParameters>>? requestConfiguration = null, CancellationToken cancellationToken = default)
+    public async ValueTask<RegistrarApiDomainCheckResponse?> CheckDomain(string accountId,
+        RegistrarApiDomainCheckRequest request,
+        Action<RequestConfiguration<DefaultQueryParameters>>? requestConfiguration = null,
+        CancellationToken cancellationToken = default)
     {
         RegistrarRequestBuilder registrar = await Get(accountId, cancellationToken).NoSync();
 
@@ -48,16 +50,18 @@ public sealed class CloudflareRegistrarUtil : ICloudflareRegistrarUtil
     }
 
     public async ValueTask<RegistrarApiRegistrationResponseCollection?> GetRegistrations(string accountId,
-        Action<RequestConfiguration<RegistrationsRequestBuilder.RegistrationsRequestBuilderGetQueryParameters>>? requestConfiguration = null,
-        CancellationToken cancellationToken = default)
+        Action<RequestConfiguration<RegistrationsRequestBuilder.RegistrationsRequestBuilderGetQueryParameters>>?
+            requestConfiguration = null, CancellationToken cancellationToken = default)
     {
         RegistrarRequestBuilder registrar = await Get(accountId, cancellationToken).NoSync();
 
         return await registrar.Registrations.GetAsync(requestConfiguration, cancellationToken).NoSync();
     }
 
-    public async ValueTask<RegistrarApiWorkflowStatusResponseSingle?> CreateRegistration(string accountId, RegistrarApiRegistrationCreateRequest request,
-        Action<RequestConfiguration<DefaultQueryParameters>>? requestConfiguration = null, CancellationToken cancellationToken = default)
+    public async ValueTask<RegistrarApiWorkflowStatusResponseSingle?> CreateRegistration(string accountId,
+        RegistrarApiRegistrationCreateRequest request,
+        Action<RequestConfiguration<DefaultQueryParameters>>? requestConfiguration = null,
+        CancellationToken cancellationToken = default)
     {
         RegistrarRequestBuilder registrar = await Get(accountId, cancellationToken).NoSync();
 
@@ -65,34 +69,42 @@ public sealed class CloudflareRegistrarUtil : ICloudflareRegistrarUtil
     }
 
     public async ValueTask<RegistrarApiRegistrationResponseSingle?> GetRegistration(string accountId, string domainName,
-        Action<RequestConfiguration<DefaultQueryParameters>>? requestConfiguration = null, CancellationToken cancellationToken = default)
+        Action<RequestConfiguration<DefaultQueryParameters>>? requestConfiguration = null,
+        CancellationToken cancellationToken = default)
     {
         RegistrarRequestBuilder registrar = await Get(accountId, cancellationToken).NoSync();
 
         return await registrar.Registrations[domainName].GetAsync(requestConfiguration, cancellationToken).NoSync();
     }
 
-    public async ValueTask<RegistrarApiWorkflowStatusResponseSingle?> UpdateRegistration(string accountId, string domainName, RegistrarApiRegistrationUpdateRequest request,
-        Action<RequestConfiguration<DefaultQueryParameters>>? requestConfiguration = null, CancellationToken cancellationToken = default)
+    public async ValueTask<RegistrarApiWorkflowStatusResponseSingle?> UpdateRegistration(string accountId,
+        string domainName, RegistrarApiRegistrationUpdateRequest request,
+        Action<RequestConfiguration<DefaultQueryParameters>>? requestConfiguration = null,
+        CancellationToken cancellationToken = default)
     {
         RegistrarRequestBuilder registrar = await Get(accountId, cancellationToken).NoSync();
 
-        return await registrar.Registrations[domainName].PatchAsync(request, requestConfiguration, cancellationToken).NoSync();
+        return await registrar.Registrations[domainName].PatchAsync(request, requestConfiguration, cancellationToken)
+                              .NoSync();
     }
 
-    public async ValueTask<RegistrarApiWorkflowStatusResponseSingle?> GetRegistrationStatus(string accountId, string domainName,
-        Action<RequestConfiguration<DefaultQueryParameters>>? requestConfiguration = null, CancellationToken cancellationToken = default)
+    public async ValueTask<RegistrarApiWorkflowStatusResponseSingle?> GetRegistrationStatus(string accountId,
+        string domainName, Action<RequestConfiguration<DefaultQueryParameters>>? requestConfiguration = null,
+        CancellationToken cancellationToken = default)
     {
         RegistrarRequestBuilder registrar = await Get(accountId, cancellationToken).NoSync();
 
-        return await registrar.Registrations[domainName].RegistrationStatus.GetAsync(requestConfiguration, cancellationToken).NoSync();
+        return await registrar.Registrations[domainName].RegistrationStatus
+                              .GetAsync(requestConfiguration, cancellationToken).NoSync();
     }
 
-    public async ValueTask<RegistrarApiWorkflowStatusResponseSingle?> GetUpdateStatus(string accountId, string domainName,
-        Action<RequestConfiguration<DefaultQueryParameters>>? requestConfiguration = null, CancellationToken cancellationToken = default)
+    public async ValueTask<RegistrarApiWorkflowStatusResponseSingle?> GetUpdateStatus(string accountId,
+        string domainName, Action<RequestConfiguration<DefaultQueryParameters>>? requestConfiguration = null,
+        CancellationToken cancellationToken = default)
     {
         RegistrarRequestBuilder registrar = await Get(accountId, cancellationToken).NoSync();
 
-        return await registrar.Registrations[domainName].UpdateStatus.GetAsync(requestConfiguration, cancellationToken).NoSync();
+        return await registrar.Registrations[domainName].UpdateStatus.GetAsync(requestConfiguration, cancellationToken)
+                              .NoSync();
     }
 }
